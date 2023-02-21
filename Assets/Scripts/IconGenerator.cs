e9d7deaa1191a97418a8a9a03f8604926374e32c
Москалик Олександр <xxteax@gmail.com>
2023-02-21 11:33:22 +0200

    Icon generator
    *Generates icon for items and automatically place them into SO
    *Place item into the scene in front of camera for shot
    *Playe scene, call screenshot method on camera

---
+++ Assets/Scripts/IconGenerator.cs
@@ -0,0 +1,84 @@
+using OM;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class IconGenerator : MonoBehaviour
+{
+    public List<GameObject> sceneObjects;
+    public List<ItemSO> dataObjects;
+    [SerializeField] private string pathFolder;
+
+    private Camera camera;
+
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    [ContextMenu("Screenshot")]
+    private void ProcessScreenshot()
+    {
+        StartCoroutine(Screenshot());
+    }
+
+    private IEnumerator Screenshot()
+    {
+        for(int i = 0; i < sceneObjects.Count; i++)
+        {
+            GameObject obj = sceneObjects[i];
+            ItemSO data = dataObjects[i];
+
+            obj.gameObject.SetActive(true);
+
+            yield return null;
+
+            TakeScreenShot($"{Application.dataPath}/{pathFolder}/{data.itemName}_Icon.png");
+
+            yield return null;
+            obj.gameObject.SetActive(false);
+
+            Sprite s = AssetDatabase.LoadAssetAtPath<Sprite>($"Assets/{pathFolder}/{data.itemName}_Icon.png");
+            if (s!=null)
+            {
+                data.icon = s;
+                EditorUtility.SetDirty(data);
+            }
+
+            yield return null;
+        }
+    }
+
+    private void TakeScreenShot(string fullPath)
+    {
+        if(camera == null)
+        {
+            camera = GetComponent<Camera>();
+        }
+        RenderTexture rt = new RenderTexture(256, 256, 24);
+        camera.targetTexture = rt;
+        Texture2D screenShot = new Texture2D(256, 256, TextureFormat.RGBA32, false);
+        camera.Render();
+        RenderTexture.active = rt;
+        screenShot.ReadPixels(new Rect(0, 0, 256, 256), 0, 0);
+        camera.targetTexture = null;
+        RenderTexture.active = null;
+
+        if(Application.isEditor)
+        {
+            DestroyImmediate(rt);
+        }
+        else
+        {
+            Destroy(rt);
+        }
+
+        byte[] bytes = screenShot.EncodeToPNG();
+        System.IO.File.WriteAllBytes(fullPath, bytes);
+#if UNITY_EDITOR
+        AssetDatabase.Refresh();
+#endif
+    }
+}
