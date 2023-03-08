89e3ee0071c0f469aef3c1a0e44494e612e4b24c
Москалик Олександр <xxteax@gmail.com>
2023-03-08 21:46:19 +0200

    First inventory iteration
    *Player can pickup items, UI will refresh as item picked up and display item

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,29 @@
+using OM;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] public List<ItemSO> items = new List<ItemSO>();
+
+    public delegate void InventoryAction();
+    public static event InventoryAction OnItemChanged;
+
+    private void Start()
+    {
+        
+    }
+
+    public void AddItem(ItemSO item)
+    {
+        items.Add(item);
+        OnItemChanged?.Invoke();
+    }
+
+    public void RemoveItem(ItemSO item) 
+    {
+        items.Remove(item);
+        OnItemChanged?.Invoke();
+    }
+}
---
+++ Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,50 @@
+using OM;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryUI : MonoBehaviour
+{
+    [SerializeField] private GameObject inventoryItemPrefab;
+    [SerializeField] private Inventory inventory;
+    private Transform inventoryPanel;
+
+    private void OnEnable()
+    {
+        Inventory.OnItemChanged += UpdateInventoryUI;
+    }
+
+    private void OnDisable()
+    {
+        Inventory.OnItemChanged -= UpdateInventoryUI;
+    }
+
+    private void Start()
+    {
+        inventoryPanel = GetComponent<Transform>();
+        UpdateInventoryUI();
+    }
+
+    public void UpdateInventoryUI()
+    {
+        // Remove any existing inventory items from the panel
+        foreach (Transform child in inventoryPanel.transform)
+        {
+            if (!child.gameObject)
+                return;
+            Destroy(child.gameObject);
+        }
+
+        // Iterate over the list of inventory items and instantiate the UI prefab for each one
+        foreach (ItemSO item in inventory.items)
+        {
+            GameObject newItem = Instantiate(inventoryItemPrefab, inventoryPanel);
+            newItem.transform.GetComponentInChildren<Image>().sprite = item.icon;
+            newItem.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = item.itemName;
+        }
+
+    }
+
+}
