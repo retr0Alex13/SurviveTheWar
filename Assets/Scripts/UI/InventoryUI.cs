31d76c6e6346e6cdf463bbd47219bc9ed86e7e2b
Москалик Олександр <xxteax@gmail.com>
2023-03-14 12:16:26 +0200

    Open/Close inventory by presing key

--- Assets/Scripts/UI/InventoryUI.cs
+++ Assets/Scripts/UI/InventoryUI.cs
@@ -3,52 +3,65 @@ using UnityEngine.EventSystems;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
-public class InventoryUI : MonoBehaviour
+
+namespace OM
 {
-    [SerializeField] private GameObject inventoryItemPrefab;
-    [SerializeField] private Inventory inventory;
-    private Transform inventoryPanel;
-
-    private void OnEnable()
+    public class InventoryUI : MonoBehaviour
     {
-        Inventory.OnItemChanged += UpdateInventoryUI;
-    }
+        [SerializeField] private GameObject inventoryItemPrefab;
+        [SerializeField] private Inventory inventory;
+        private bool isOpened = true;
+        private Transform inventoryPanel;
 
-    private void OnDisable()
-    {
-        Inventory.OnItemChanged -= UpdateInventoryUI;
-    }
-
-    private void Start()
-    {
-        inventoryPanel = GetComponent<Transform>();
-        UpdateInventoryUI();
-    }
-
-    private void Update()
-    {
-
-    }
-
-    public void UpdateInventoryUI()
-    {
-        // Remove any existing inventory items from the panel
-        foreach (Transform child in inventoryPanel.transform)
+        private void OnEnable()
         {
-            if (!child.gameObject)
-                return;
-            Destroy(child.gameObject);
+            Inventory.OnItemChanged += UpdateInventoryUI;
         }
 
-        // Iterate over the list of inventory items and instantiate the UI prefab for each one
-        foreach (ItemSO item in inventory.items)
+        private void OnDisable()
         {
-            GameObject newItem = Instantiate(inventoryItemPrefab, inventoryPanel);
-            newItem.transform.GetComponentInChildren<Image>().sprite = item.icon;
-            newItem.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = item.itemName;
+            Inventory.OnItemChanged -= UpdateInventoryUI;
+        }
+
+        private void Start()
+        {
+            inventoryPanel = GetComponent<Transform>();
+            UpdateInventoryUI();
+        }
+
+        private void Update()
+        {
+
+        }
+
+        public void UpdateInventoryUI()
+        {
+            // Remove any existing inventory items from the panel
+            foreach (Transform child in inventoryPanel.transform)
+            {
+                if (!child.gameObject)
+                    return;
+                Destroy(child.gameObject);
+            }
+
+            // Iterate over the list of inventory items and instantiate the UI prefab for each one
+            foreach (ItemSO item in inventory.items)
+            {
+                GameObject newItem = Instantiate(inventoryItemPrefab, inventoryPanel);
+                newItem.transform.GetComponentInChildren<Image>().sprite = item.icon;
+                newItem.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = item.itemName;
+            }
+        }
+
+        public void ControlInventoryWindow(InputAction.CallbackContext ctx)
+        {
+            if(ctx.performed)
+            {
+                gameObject.SetActive(!gameObject.activeSelf);
+            }
         }
     }
-
 
 }
