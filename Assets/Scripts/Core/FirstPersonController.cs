6c744a09f98f1339b94718e46b11dd18485fba0c
Oleksandr Moskalyk <alexundercover191@gmail.com>
2023-05-10 23:39:22 +0300

    Do adjustments for sound manager

--- Assets/Scripts/Core/FirstPersonController.cs
+++ Assets/Scripts/Core/FirstPersonController.cs
@@ -158,13 +158,25 @@ namespace StarterAssets
         private void Move()
         {
             // set target speed based on move speed, sprint speed and if sprint is pressed
-            float targetSpeed = _input.IsSprinting() ? sprintSpeed : moveSpeed;
+            //float targetSpeed = _input.IsSprinting() ? sprintSpeed : moveSpeed;
+            float targetSpeed;
+            if(_input.IsSprinting())
+            {
+                targetSpeed = sprintSpeed;
+            }
+            else
+            {
+                targetSpeed = moveSpeed;
+            }
 
             // a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
 
             // note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
             // if there is no input, set the target speed to 0
-            if (_input.GetMove() == Vector2.zero) targetSpeed = 0.0f;
+            if (_input.GetMove() == Vector2.zero)
+            {
+                targetSpeed = 0.0f;
+            }
 
             // a reference to the players current horizontal velocity
             float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
