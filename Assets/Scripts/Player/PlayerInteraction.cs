6140b61424635c5101cf910b1d27427c350fe80e
Oleksandr Moskalyk <alexundercover191@gmail.com>
2023-05-10 23:40:40 +0300

    Add hint panel for interactable items

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -1,4 +1,6 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace OM
 {
@@ -8,16 +10,25 @@ namespace OM
         [SerializeField, Tooltip("Distance for player interaction")]
         private float interactDistance = 4f;
         private int ineteractLayerMask = 1 << 10;
+        [SerializeField] private GameObject hintPanel;
+        [SerializeField] private TextMeshProUGUI hintText;
 
-        // for storing interactable interface
+        private PlayerInput playerInput;
+
+        // for storing selectable interface
         private Transform selection;
+
+        private void Awake()
+        {
+            playerInput = GetComponent<PlayerInput>();
+        }
 
         void Update()
         {
-            HandleInteraction();
+            HandleSelection();
         }
 
-        private void HandleInteraction()
+        private void HandleSelection()
         {
             // if selecttion was setted
             if (selection != null)
@@ -30,19 +41,60 @@ namespace OM
                 selection = null;
             }
 
-            var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
             RaycastHit hit;
+            Ray ray = FireRay();
+            var pickUpKey = playerInput.actions["Pickup"].GetBindingDisplayString();
+            var interactKey = playerInput.actions["Interact"].GetBindingDisplayString();
 
             if (Physics.Raycast(ray, out hit, interactDistance, ineteractLayerMask))
             {
                 // get a hit transform
                 Transform selected = hit.transform;
-                if (hit.transform.TryGetComponent(out ISelectable interactable))
+                if (hit.transform.TryGetComponent(out ISelectable selectable))
                 {
-                    interactable.Highlight();
+                    selectable.Highlight();
                 }
+                if (hit.transform.TryGetComponent(out IInteractable interactable))
+                {
+                    if (interactable.IsPickable)
+                    {
+                        hintText.text = $"Натисність \"{pickUpKey}\" щоб підібрати";
+                        hintPanel.SetActive(true);
+                    }
+                    else
+                    {
+                        hintText.text = $"Натисність \"{interactKey}\" щоб взаємодіяти";
+                        hintPanel.SetActive(true);
+                    }
+                }
+               
                 // set selection tranform to hit interface transofm
                 selection = selected;
+            }
+            else
+            {
+                hintPanel.SetActive(false);
+            }
+        }
+
+        private static Ray FireRay()
+        {
+            return Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
+        }
+
+        public void Interact(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                RaycastHit hit;
+                Ray ray = FireRay();
+                if (Physics.Raycast(ray, out hit, interactDistance, ineteractLayerMask))
+                {
+                    if (hit.transform.TryGetComponent(out IInteractable interactable))
+                    {
+                        interactable.Interact();
+                    }
+                }
             }
         }
     }
