e15208c5c78700de20d928b3a2232a2345074e60
Oleksandr Moskalyk <kn191_mos@student.ztu.edu.ua>
2023-05-30 16:05:45 +0300

    Add Generator script

---
+++ Assets/Scripts/PowerGeneratorController.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OM
+{
+    public class PowerGeneratorController : MonoBehaviour, IInteractable
+    {
+        public bool IsPickable { get; set; }
+
+        [SerializeField] private ParticleSystem smokeParticle;
+        [SerializeField] private float fuel;
+        [SerializeField] private List<Light> lights;
+        private bool isTurnedOn;
+
+        private void Update()
+        {
+            if (!isTurnedOn)
+            {
+                return;
+            }
+            else
+            {
+                // Start to subtract fuel;
+                fuel -= Time.deltaTime / 5;
+            }
+            Debug.Log($"Generator fuel: {fuel}");
+            if (!CheckFuel())
+            {
+                StopGenerator();
+                fuel = 0;
+            }
+        }
+
+        public void Interact()
+        {
+            ControlGenerator();
+        }
+        
+        private void ControlGenerator()
+        {
+            if (!CheckFuel() && !isTurnedOn)
+            {
+                Debug.Log("Can't start without fuel");
+                return;
+            }
+            else if (CheckFuel() && !isTurnedOn)
+            {
+                StartGenerator();
+            }
+            else
+            {
+                StopGenerator();
+            }
+        }
+
+        private void StartGenerator()
+        {
+            Debug.Log("Starting..");
+            isTurnedOn = true;
+            
+            // Play particles
+            if (!smokeParticle.isPlaying)
+            {
+                smokeParticle.Play();
+            }
+            
+            // Play sound
+            SoundManager.Instance.PlaySound("GeneratorRunning", transform.position);
+
+            // Set light enabled = true in light list
+            foreach (Light light in lights)
+            {
+                light.enabled = true;
+            }
+        }
+
+        private void StopGenerator()
+        {
+            Debug.Log("Stopping generator");
+            isTurnedOn = false;
+            
+            // Stop particles
+            if (smokeParticle.isPlaying)
+            {
+                smokeParticle.Stop();
+            }
+            
+            // Stop sound
+            SoundManager.Instance.StopSound("GeneratorRunning");
+            
+            // Set light enabled = false in light list
+            foreach (Light light in lights)
+            {
+                light.enabled = false;
+            }
+        }
+
+        public void AddFuel(float fuelAmount)
+        {
+            fuel += fuelAmount;
+        }
+
+        private bool CheckFuel()
+        {
+            if (fuel > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
